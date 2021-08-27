be4994e9d8eb07d29c0ec1baca27686170e4c549
dreingineer <andreimartinbaquino@gmail.com>
2021-08-27 13:36:20 +0800

    added boiler plate code for salaryCalulator function

---
+++ src/Solution.java
@@ -0,0 +1,13 @@
+package src;
+
+public class Solution {
+	
+	public static void salaryCalculator() {
+		System.out.println("function called!");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		salaryCalculator();
+	}
+}
